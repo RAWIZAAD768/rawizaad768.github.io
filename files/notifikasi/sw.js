fb0efc4416a75d7d6853bc85a331f7aa060e5258
RAWIZAAD768 <wildanza2784@gmail.com>
2025-12-25 22:13:15 +0700

    Update sw.js

--- files/notifikasi/sw.js
+++ files/notifikasi/sw.js
@@ -1,16 +1,8 @@
-// Service Worker sederhana untuk notifikasi
 self.addEventListener('install', event => {
     console.log('Service Worker terpasang.');
-    self.skipWaiting(); // aktif segera
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
     console.log('Service Worker aktif.');
-});
-
-self.addEventListener('notificationclick', event => {
-    event.notification.close();
-    event.waitUntil(
-        clients.openWindow('https://rawizaad768.github.io/dataWeb') // buka halaman target
-    );
 });
